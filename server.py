fc5ba9975454187e0ddb12e5f964a9486e8b88ed
xoxoedg <rugyasin@gmail.com>
2025-02-22 13:59:36 +0100

    Server Setup

---
+++ server.py
@@ -0,0 +1,30 @@
+from socket import *
+
+server_port = 34000
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(("0.0.0.0", server_port))
+serverSocket.listen(1)
+
+
+while True:
+
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+    ip, port = addr
+    print("Accepting request from address: " + ip)
+    try:
+        message = connectionSocket.recv(1024).decode()
+        print(message)
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.read()
+        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+        connectionSocket.send("\r\n".encode())
+        connectionSocket.sendall(outputdata.encode())
+        connectionSocket.close()
+    except IOError:
+        connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+        connectionSocket.close()
+    
+serverSocket.close()
+sys.exit()
