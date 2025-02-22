615f492780c2ba1ad9d333a46b1ddad751b8193f
xoxoedg <rugyasin@gmail.com>
2025-02-22 14:43:14 +0100

    Add loggerr

--- server.py
+++ server.py
@@ -1,17 +1,22 @@
 from socket import *
+import logging
+
+from tech.logger import Logger
+
+logger = Logger(__name__).get_logger()
 
 server_port = 34000
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(("0.0.0.0", server_port))
 serverSocket.listen(1)
 
-
 while True:
 
-    print('Ready to serve...')
+    logger.info('Ready to serve...')
     connectionSocket, addr = serverSocket.accept()
     ip, port = addr
-    print("Accepting request from address: " + ip)
+
+    logger.info("Accepting request from address: " + ip)
     try:
         message = connectionSocket.recv(1024).decode()
         print(message)
@@ -22,7 +27,8 @@ while True:
         connectionSocket.send("\r\n".encode())
         connectionSocket.sendall(outputdata.encode())
         connectionSocket.close()
-    except IOError:
+    except IOError as e:
+        logging.error("Error while sending data" + e.message)
         connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
         connectionSocket.close()
     
